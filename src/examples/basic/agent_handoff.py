147a22578f7e6103e65be300d6877a422ba66c5f
Papilon <leeskiikseel@outlook.com>
2025-01-03 12:56:47 +0800

    update

---
+++ src/examples/basic/agent_handoff.py
@@ -0,0 +1,37 @@
+from swarm import Swarm,Agent
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+import pprint
+
+load_dotenv()
+
+api_key = os.getenv('DEEPSEEK_API_KEY')
+
+openai_client = OpenAI(api_key=api_key,base_url='https://api.deepseek.com')
+
+
+client = Swarm(client=openai_client)
+
+english_agent = Agent(name='English Agent',model='deepseek-chat',instructions='You only speak English.')
+
+spanish_agent = Agent(name='Spanish Agent',model='deepseek-chat',instructions='You only speak Spanish.')
+
+Mandarin_agent = Agent(name='Mandarin Agent',model='deepseek-chat',instructions='你只能使用简体中文进行交流。')
+
+def transfer_to_spanish_agent():
+    return spanish_agent
+
+def transfer_to_mandarin_agent():
+    return Mandarin_agent
+
+english_agent.functions.append(transfer_to_spanish_agent)
+english_agent.functions.append(transfer_to_mandarin_agent)
+
+
+# messages = [{'role':'user','content': "Hola. ¿Como estás?"}]
+messages = [{'role':'user','content': "你好，你是谁?"}]
+
+responses = client.run(agent=english_agent,messages=messages)
+
+pprint.pprint(responses.messages)
---
+++ src/main.py
@@ -0,0 +1,47 @@
+from swarm import Swarm, Agent
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("DEEPSEEK_API_KEY")
+
+open_client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.deepseek.com",
+)
+
+client = Swarm(client=open_client)
+
+def transfer_to_agent_b():
+    return agent_b
+  
+def transfer_to_agent_c():
+    return agent_c
+
+
+agent_a = Agent(
+    name="主理人",
+    model="deepseek-chat",
+    instructions="你是一位乐于助人的机器人。",
+    functions=[transfer_to_agent_b,transfer_to_agent_c],
+)
+
+agent_b = Agent(
+    name="英语专家",
+    model="deepseek-chat",
+    instructions="请只使用英语进行回复.",
+)
+agent_c = Agent(
+    name="数学专家",
+    model="deepseek-chat",
+    instructions="精通数学运算，回复使用法语.",
+)
+
+response = client.run(
+    agent=agent_a,
+    messages=[{"role": "user", "content": "我想要求解数学问题，请帮我找一位精通数学的专家，解决3+4+5=?"}],
+)
+
+print(response.messages[-1]["content"])
